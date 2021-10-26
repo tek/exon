25287ee58aa4f0df89fc47982aabc9f80c26d841
Torsten Schmits <git@tryp.io>
2021-10-26 23:31:47 +0200

    bump flatparse

--- flake.nix
+++ flake.nix
@@ -6,20 +6,23 @@
   outputs = { hix, ... }:
   let
     common = { hackage, source, jailbreak, ... }: {
-      flatparse = hackage "0.2.2.0" "030daxpl0g7p5szmai6w1imgvvy11gx9yp5b1iaml3069s95qdqk";
+      flatparse = hackage "0.3.1.0" "15nx2p08pqka0136xfppw344a60rn3fvsx4adiz15k37cyj25zi2";
+    };
+
+    relude = { hackage, ... }: {
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
 
     main = { hackage, source, ... }: {
-      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
     };
 
   in hix.flake {
     base = ./.;
     packages.exon = "packages/exon";
-    overrides = [common main];
-    compatOverrides = common;
-    compatVersions = ["8104" "884"];
+    overrides = [common relude main];
+    compatOverrides = { all = common; ghc901 = relude; };
+    # compatVersions = ["8107" "884"];
     versionFile = "ops/hpack/shared/meta.yaml";
     ghcid.easy-hls = false;
   };
