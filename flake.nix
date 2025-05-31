74902fdbc40eff833c456a0f93759bb208ae28f5
Torsten Schmits <git@tryp.io>
2025-05-31 19:24:24 +0200

    update build for compatible revision app

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Customizable quasiquote interpolation";
 
-  inputs.hix.url = "git+https://git.tryp.io/tek/hix?rev=e5b8b98097be9eb161f4d832ac8edf8b1d353cc5";
+  inputs.hix.url = "git+https://git.tryp.io/tek/hix";
 
   outputs = {hix, ...}: hix.lib.pro ({config, lib, ...}: {
     ghcVersions = ["ghc92" "ghc94" "ghc96" "ghc98" "ghc910" "ghc912"];
@@ -12,6 +12,7 @@
       src = ./packages/exon;
 
       cabal = {
+        language = "GHC2021";
         license = "BSD-2-Clause-Patent";
         license-file = "LICENSE";
         author = "Torsten Schmits";
@@ -63,16 +64,11 @@
       enable = true;
       lower.enable = true;
       latest.compiler = "ghc912";
-      latest.envs.solverOverrides = {hackage, jailbreak, notest, ...}: {
-        boring = jailbreak;
-        generic-deriving = jailbreak;
-        happy = notest;
-        hashable = jailbreak;
-        hedgehog = jailbreak;
-        incipit-base = jailbreak;
-        lifted-base = notest;
-        th-abstraction = hackage "0.7.1.0" "09wr7x9bpzyrys8id1mavk9wvqhh2smxdkfwi82kpcycm7a1z7sx";
-      };
+    };
+
+    hackage.repos."hackage.haskell.org" = {
+      user = "tek";
+      password = "";
     };
 
     envs.dev.overrides = {bench, ...}: {
@@ -101,6 +97,10 @@
       lifted-base = notest;
       th-abstraction = hackage "0.7.1.0" "09wr7x9bpzyrys8id1mavk9wvqhh2smxdkfwi82kpcycm7a1z7sx";
     };
+
+    ui.experimental.managed-maint = true;
+
+    internal.hixCli.dev = true;
 
   });
 }
